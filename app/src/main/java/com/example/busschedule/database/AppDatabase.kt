4badc737da38838ddaf5938e97553bfd6d46c658
Sharlene Kumbhar <shakumbhar@gmail.com>
2021-10-25 15:10:51 +0530

    using data from room

---
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -0,0 +1,24 @@
+package com.example.busschedule.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun scheduleDao():ScheduleDao
+    @Database(entities = arrayOf(Schedule::class),version = 1)
+    companion object{
+       @Volatile
+       private var INSTANCE: AppDatabase?=null
+        fun getDatabase(context:Context):AppDatabase{
+            return INSTANCE?: synchronized(this){
+                val instance=Room.databaseBuilder(context,AppDatabase::class.java,"app_database")
+                    .createFromAsset("database/bus_schedule.db")
+                    .build()
+                INSTANCE=instance
+                instance
+            }
+        }
+    }
+}
